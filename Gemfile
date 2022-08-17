69ce7030b4995eba9ab141cb727e0f39949fc036
dsantillan <dsantillan@kssoluciones.com>
2022-08-16 20:47:48 -0500

    Creacion de gema

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source "https://rubygems.org"
 
+gem 'api-fedex'
 # Specify your gem's dependencies in api-fedex.gemspec
 gemspec
--- lib/api/fedex.rb
+++ lib/api/fedex.rb
@@ -2,11 +2,15 @@ require "api/fedex/version"
 
 module Fedex
   class Rates
-    def self.get(credentials, quote_params)
-      puts "#################"
-      puts credentials
-      puts quote_params
-      puts "#################"
+    def self.get
+      begin
+        datos = RestClient.get 'https://pokeapi.co/api/v2/pokemon/?offset=1000&limit=1000', {'Content-Type': 'application/json', 'Accept': 'application/json'}
+        return JSON.parse datos.to_str
+      rescue RestClient::ExceptionWithResponse => e
+        return JSON.parse(e.response).to_json
+      rescue => e
+        return (44).to_json
+      end
     end
   end
 
