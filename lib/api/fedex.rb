5e957d831395dfc73ceb7b8aa86bc5ba7c8e99ec
dsantillan <dsantillan@kssoluciones.com>
2022-08-16 20:10:45 -0500

    Creacion de gema

---
+++ lib/api/fedex.rb
@@ -0,0 +1,13 @@
+require "api/fedex/version"
+
+module Fedex
+  class Rates
+    def self.get(credentials, quote_params)
+      puts "#################"
+      puts credentials
+      puts quote_params
+      puts "#################"
+    end
+  end
+
+end
---
+++ lib/api/fedex/version.rb
@@ -0,0 +1,5 @@
+module Api
+  module Fedex
+    VERSION = "0.1.0"
+  end
+end
